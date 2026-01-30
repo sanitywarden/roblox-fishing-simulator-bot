5e4528ae31c7146cedc8b468eb6752888a27cafb
sanitywarden <jedlinski.szymon@gmail.com>
2026-01-30 17:38:54 +0100

    Made the bot sell fish if near a stall

--- macos.py
+++ macos.py
@@ -4,19 +4,39 @@ import yaml
 import mss
 import numpy as np
 import pyautogui
-from pynput import keyboard
 import Quartz.CoreGraphics as CG
+from pynput import keyboard
+
+# After selling fish there is issue with continuing
+# Automatic reset
 
 # Native Mac click
 def mac_click(x, y, duration):
     point = CG.CGPointMake(x, y)
+    
+    move = CG.CGEventCreateMouseEvent(None, CG.kCGEventMouseMoved, point, CG.kCGMouseButtonLeft)
+    CG.CGEventPost(CG.kCGHIDEventTap, move)
+
+    time.sleep(0.05)
+    
     down = CG.CGEventCreateMouseEvent(None, CG.kCGEventLeftMouseDown, point, CG.kCGMouseButtonLeft)
     up = CG.CGEventCreateMouseEvent(None, CG.kCGEventLeftMouseUp, point, CG.kCGMouseButtonLeft)
-    
+
     CG.CGEventPost(CG.kCGHIDEventTap, down)
     time.sleep(duration)
     CG.CGEventPost(CG.kCGHIDEventTap, up)
+    time.sleep(0.1)
+
+# Code for E on macOS keyboard
+MAC_keyboard_E = 14
+
+def mac_keypress(key_code, duration):
+    push = CG.CGEventCreateKeyboardEvent(None, key_code, True)
+    release = CG.CGEventCreateKeyboardEvent(None, key_code, False)
+    
+    CG.CGEventPost(CG.kCGHIDEventTap, push)
     time.sleep(duration)
+    CG.CGEventPost(CG.kCGHIDEventTap, release)
 
 class KeyTracker:
     def __init__(self):
@@ -78,6 +98,7 @@ quit_script_hotkey           = config['quit_script_hotkey']
 pause_script_hotkey          = config['pause_script_hotkey']
 reset_script_hotkey          = config['reset_script_hotkey']
 show_info_hotkey             = config['show_info_hotkey']
+force_sell_fish_hotkey       = config['sell_fish_hotkey']
 fishing_minigame_click_min_s = float(config['fishing_minigame_click_min_s'])
 fishing_minigame_click_max_s = float(config['fishing_minigame_click_max_s'])
 detect_range_bubble_x        = int(config['detect_range_bubble_x'])
@@ -113,6 +134,9 @@ def check_area(screenshot, center_x, center_y, range_x, range_y, r_bounds, g_bou
 def click(x, y):
     mac_click(x, y, duration=random.uniform(0.1, 0.2))
 
+def click_random(x, y, min_time = 0.1, max_time = 0.2):
+    mac_click(x + random.uniform(-8, 8), y + random.uniform(-8, 8), random.uniform(min_time, max_time))
+
 def click_timed(x, y, min_time, max_time):
     mac_click(x, y, duration=random.uniform(min_time, max_time))
 
@@ -133,6 +157,51 @@ def check_backpack_is_full():
     return check_area(screenshot, middle_screen_x, y_pos, detect_range_backpack_full_x, detect_range_backpack_full_y,
                       (230, 245), (40, 55), (95, 105))
 
+def find_color_in_roi(r_bounds, g_bounds, b_bounds, x_ratio, y_ratio, box_ratio=0.1):
+    with mss.mss() as sct:
+        monitor = sct.monitors[1]
+        full_img = np.array(sct.grab(monitor))
+        f_h, f_w = full_img.shape[:2]
+
+        center_x, center_y = int(f_w * x_ratio), int(f_h * y_ratio)
+        offset_x, offset_y = int(f_w * box_ratio), int(f_h * box_ratio)
+
+        x1, x2 = max(0, center_x - offset_x), min(f_w, center_x + offset_x)
+        y1, y2 = max(0, center_y - offset_y), min(f_h, center_y + offset_y)
+
+        roi = full_img[y1:y2, x1:x2]
+
+        mask = (
+            (roi[:, :, 2] >= r_bounds[0]) & (roi[:, :, 2] <= r_bounds[1]) &
+            (roi[:, :, 1] >= g_bounds[0]) & (roi[:, :, 1] <= g_bounds[1]) &
+            (roi[:, :, 0] >= b_bounds[0]) & (roi[:, :, 0] <= b_bounds[1])
+        )
+
+        coords = np.where(mask)
+
+        if len(coords[0]) > 0:
+            avg_y = int(np.mean(coords[0]))
+            avg_x = int(np.mean(coords[1]))
+            
+            phys_y = avg_y + y1
+            phys_x = avg_x + x1
+
+            return int(phys_x / scale), int(phys_y / scale)
+            
+    return None
+
+def find_sell_button_coords():
+    r_range = (80, 120)
+    g_range = (140, 200)
+    b_range = (200, 255)
+    return find_color_in_roi(r_range, g_range, b_range, 0.735, 0.43, box_ratio=0.1)
+
+def find_sell_everything_button_coords():
+    r_range = (110, 140)
+    g_range = (210, 255)
+    b_range = (70, 130)
+    return find_color_in_roi(r_range, g_range, b_range, 0.60, 0.405, box_ratio=0.05)
+
 def print_with_time(message):
     print(f"[{time.strftime('%H:%M:%S')}] {message}")
 
@@ -140,11 +209,16 @@ def print_with_time_debug(message):
     if debug_mode: 
         print_with_time(f"[debug] {message}")
 
-time_till_reset_state  = reset_state_cooldown_s
+time_till_reset_state  = reset_state_cooldown_s 
 is_in_fishing_minigame = False
 thrown_hook            = False
 paused_script          = False
+force_sell_fish        = False
+in_shop                = False
+in_sell_screen         = False
+in_sell_everything     = False
 script_caught_fish     = 0
+current_fish           = 0
 start_timestamp        = 0
 
 if debug_mode:
@@ -188,11 +262,34 @@ while not tracker.is_pressed(quit_script_hotkey):
     if tracker.is_pressed(reset_script_hotkey):
         print_with_time("Manual reset state")
         time_till_reset_state = reset_state_cooldown_s
-        thrown_hook = False
-        is_in_fishing_minigame = False
-        paused_script = False
+        paused_script          = False
+        thrown_hook            = check_bubbles_on_screen()
+        is_in_fishing_minigame = check_fishing_minigame_arc_is_on_screen()
+        in_shop                = find_sell_button_coords()
+        in_sell_everything     = find_sell_everything_button_coords() is not None
+        force_sell_fish        = False
         time.sleep(1)
         continue
+
+    # 2.1 Periodic reset
+    if time_till_reset_state == 0:
+        time_till_reset_state = reset_state_cooldown_s
+        
+        thrown_hook            = check_bubbles_on_screen()
+        is_in_fishing_minigame = check_fishing_minigame_arc_is_on_screen()
+        in_shop                = find_sell_button_coords()
+        in_sell_everything     = find_sell_everything_button_coords() is not None
+        force_sell_fish        = False
+
+
+        print_with_time_debug(f"===== Performing automatic reset done every {reset_state_cooldown_s} seconds =====")
+        print_with_time_debug(f"thrown_hook: {thrown_hook}")
+        print_with_time_debug(f"is_in_fishing_minigame: {is_in_fishing_minigame}")
+        print_with_time_debug(f"in_shop: {in_shop}")
+        print_with_time_debug(f"in_sell_everything: {in_sell_everything}")
+        print_with_time_debug("==========")
+
+        time.sleep(1)
 
     # 3. Pause Script
     if tracker.is_pressed(pause_script_hotkey):
@@ -204,44 +301,89 @@ while not tracker.is_pressed(quit_script_hotkey):
         time.sleep(1)
         continue
 
+    if tracker.is_pressed(force_sell_fish_hotkey):
+        force_sell_fish = True
+        if force_sell_fish:
+            print_with_time_debug("Force sell fish")
+
     # =============================================
     # Throw hook or start fishing minigame
     # =============================================
 
+    # Start fishing minigame
     bubbles_visible = check_bubbles_on_screen()
     if bubbles_visible and not is_in_fishing_minigame:
-        click(middle_screen_x, middle_screen_y)
+        click_random(middle_screen_x, middle_screen_y)
     
         if check_fishing_minigame_arc_is_on_screen():
             print_with_time_debug("Start fishing minigame")
             is_in_fishing_minigame = True
 
-    if not bubbles_visible and not is_in_fishing_minigame and not thrown_hook:
-        click(middle_screen_x, middle_screen_y)
-        
-        if check_backpack_is_full():
-            print_with_time("Pausing the script - backpack is full")
-            paused_script = True
+    # Throw hook
+    elif not bubbles_visible and not is_in_fishing_minigame and not thrown_hook:
+        click_random(middle_screen_x, middle_screen_y)
+
+        backpack_full = check_backpack_is_full()
+        if force_sell_fish or check_backpack_is_full():
+
+            # Try opening the shop a few times to see if it's nearby
+            mac_keypress(MAC_keyboard_E, 0.1)
+
+            # Wait for menu to open
+            time.sleep(1)
+
+            # If the menu opened, it should be visible by now
+            # Check if there is the blue/cyan sell button on the screen
+            button_coords = find_sell_button_coords()
+            if (not in_shop and button_coords is not None):
+                in_shop = True
+
+                # Might be fishing, click to stop fishing
+                click(middle_screen_x, middle_screen_y)
+
+                x1 = button_coords[0]
+                y1 = button_coords[1]
+                
+                mac_click(x1, y1, 0.1)
+                time.sleep(1)
+
+                sell_everything_button_coords = find_sell_everything_button_coords()
+                if (sell_everything_button_coords is not None):
+                    in_sell_everything = True
+                    x2 = sell_everything_button_coords[0]
+                    y2 = sell_everything_button_coords[1]
+                    
+                    # Click button to sell everything
+                    mac_click(x2, y2, 0.1)
+                    time.sleep(0.1)
+
+                    # Click button to confirm the sale
+                    mac_click(x2, y2, 0.1)
+                    time.sleep(0.1)
+
+                    in_sell_everything = False
+                
+                current_fish = 0
+
+                thrown_hook = False
+                is_in_fishing_minigame = False
+
+                in_shop = False
+                force_sell_fish = False
+
         else:
             print_with_time_debug("Throw hook")
             thrown_hook = True
+            if time_till_reset_state < reset_state_cooldown_s:
+                time_till_reset_state += reset_state_cooldown_s
 
-    # 5. Automatic reset state
-    if not thrown_hook and not bubbles_visible and not is_in_fishing_minigame and not check_fishing_minigame_arc_is_on_screen():
+    else:
         time_till_reset_state -= 1
-        if time_till_reset_state <= 0:
-            print_with_time_debug(f"Automatic reset every {reset_state_cooldown_s}")
-            time_till_reset_state = reset_state_cooldown_s
-            thrown_hook = False
-            is_in_fishing_minigame = False
-            paused_script = False
         time.sleep(1)
-        continue
 
-    # 6. Play minigame
+    # Play minigame
     while is_in_fishing_minigame and thrown_hook and check_fishing_minigame_arc_is_on_screen():
-        click_timed(middle_screen_x, middle_screen_y, fishing_minigame_click_min_s, fishing_minigame_click_max_s)
-        if tracker.is_pressed(quit_script_hotkey): break
+        click_random(middle_screen_x, middle_screen_y, fishing_minigame_click_min_s, fishing_minigame_click_max_s)
 
     if is_in_fishing_minigame and thrown_hook and not check_fishing_minigame_arc_is_on_screen():
         print_with_time_debug("End fishing minigame")
@@ -249,5 +391,6 @@ while not tracker.is_pressed(quit_script_hotkey):
         thrown_hook = False
         time_till_reset_state = reset_state_cooldown_s
         script_caught_fish += 1
+        current_fish += 1
 
     time.sleep(0.1)
